df4f89eb0a63b734add38cd4cec38360e1d2f363
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-12-29 22:45:26 +0600

    added home page, install material UI and Tailwindcss

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,10 +1,17 @@
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            home
-        </div>
-    );
+  return (
+    <div className="w-full min-h-screen">
+      <div className=" pt-52 text-center min-h-screen ">
+        <h1 className="text-4xl font-bold mb-6">My Assignment</h1>
+        <Link to={"/dashboard"}>
+        <Button variant="contained">Go to Dashboard</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
