203011eb75533960c90e8b05e3efdc5784d43921
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-12-29 22:01:27 +0600

    create home page

--- src/route/routers/Routers.jsx
+++ src/route/routers/Routers.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../../Pages/root/Root";
+import Home from "../../Pages/home/Home";
 
 const route = createBrowserRouter([
     {
         path:"/",
-        element:<Root/>
+        element:<Root/>,
+        children:[
+            {
+                path:'/',
+                element:<Home/>
+            }
+        ]
     }
 ])
 
